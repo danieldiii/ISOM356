9c8b4c933533b7110234c8b64dafbf9517cc7df1
Frank Fu <40412478+frankfu07@users.noreply.github.com>
2019-04-21 11:01:56 -0400

    Add files via upload

---
+++ Police.java
@@ -0,0 +1,29 @@
+package typingGame;
+
+import acm.graphics.GCompound;
+import acm.graphics.GImage;
+
+public class Police extends GCompound{
+	private GImage image;
+    Map map;
+    public Police(Map map) {
+        image = new GImage("/Users/Daniel/desktop/police.png");
+        image.setSize(60,60);
+        this.map = map;
+        this.add(image);
+        this.setLocation(50,50);
+    }
+    
+    public void move() {
+        map.hasPassedPolice((int)this.getY()/50,(int)this.getX()/50);
+        switch(map.NextStepDirPolice((int)this.getY()/50,(int)this.getX()/50)) {
+        case 0:this.setLocation(this.getX(), this.getY()-50);break;
+        case 1:this.setLocation(this.getX()+50, this.getY());break;   
+        case 2:this.setLocation(this.getX(), this.getY()+50);break;
+        case 3:this.setLocation(this.getX()-50, this.getY());break;        
+        }
+    }
+    public void backword() {
+    	
+    }
+}
